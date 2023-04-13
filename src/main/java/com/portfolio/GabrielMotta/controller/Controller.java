d68a453182b39abd0c869847a77f1ddb5ec22ed1
mottitag <mottitag@gmail.com>
2023-04-13 10:19:27 -0300

    registro person exitoso. Falta crear el resto de las interfaces

--- src/main/java/com/portfolio/GabrielMotta/controller/Controller.java
+++ src/main/java/com/portfolio/GabrielMotta/controller/Controller.java
@@ -1,8 +1,30 @@
 package com.portfolio.GabrielMotta.controller;
 
+import com.portfolio.GabrielMotta.model.Person;
+import com.portfolio.GabrielMotta.service.IPersonService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    
+    @Autowired
+    private IPersonService persServ;
+    
+    @PostMapping("/new/person")
+    public void addPerson (@RequestBody Person per){
+        persServ.createPerson(per);
+    }
+    
+    @GetMapping ("/ver/person")
+    @ResponseBody
+    public List<Person>bringPersons(){
+        return persServ.findPersons();
+    }
     
 }
--- src/main/java/com/portfolio/GabrielMotta/service/PersonService.java
+++ src/main/java/com/portfolio/GabrielMotta/service/PersonService.java
@@ -4,7 +4,9 @@ import com.portfolio.GabrielMotta.model.Person;
 import com.portfolio.GabrielMotta.repository.PersonRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PersonService implements IPersonService{
     
     @Autowired
