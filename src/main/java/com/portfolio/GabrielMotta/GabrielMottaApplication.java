e3d1322c4201ba5dd2fd3a513922f4482d5e94c0
mottitag <mottitag@gmail.com>
2023-05-11 12:19:23 -0300

    Deploy con CORS sin JWT

--- src/main/java/com/portfolio/GabrielMotta/GabrielMottaApplication.java
+++ src/main/java/com/portfolio/GabrielMotta/GabrielMottaApplication.java
@@ -18,7 +18,7 @@ public class GabrielMottaApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/**").allowedOrigins("https://portfolio-backend-yh5v.onrender.com");
 			}
 		};
 	}
